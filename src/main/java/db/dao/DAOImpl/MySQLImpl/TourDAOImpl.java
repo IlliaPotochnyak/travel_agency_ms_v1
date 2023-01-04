1086af2766593a334fd2d892dfe958ad69d4d532
IlyaPotochnyak <iluha.diego@gmail.com>
2023-01-04 17:05:05 +0200

    add JOIN to display tour type names in getSortedTours

--- src/main/java/db/dao/DAOImpl/MySQLImpl/TourDAOImpl.java
+++ src/main/java/db/dao/DAOImpl/MySQLImpl/TourDAOImpl.java
@@ -54,21 +54,22 @@ public class TourDAOImpl implements TourDAO {
     @Override
     public List<Tour> getSortedTours(String tourType, String price, String personNumber, String hotelType) throws DatabaseException {
         List<Tour> tourList = new ArrayList<>();
+
         String query = "SELECT tour.id, tour.name, tour.description, tour.persons_number, tour.price, tour.hot, \n" +
                 "tour_type.tour_type, hotel_type.star_rate \n" +
                 "FROM ((tour\n" +
                 "INNER JOIN tour_type ON tour.tour_type_id=tour_type.id)\n" +
-                "INNER JOIN hotel_type ON tour.hotel_type_id=hotel_type.star_rate) " +
-                "WHERE tour_type.tour_type=? " +
-                "AND price<=? AND persons_number=? AND hotel_type.star_rate>=? " +
+                "INNER JOIN hotel_type ON tour.hotel_type_id=hotel_type.id) WHERE tour_type=? AND price<=? " +
+                "AND persons_number=? " +
+                "AND star_rate>=? " +
                 "ORDER BY hot DESC;";
         try (Connection con = DataSource.getConnection();
              PreparedStatement pstmnt = con.prepareStatement(query)){
+
             pstmnt.setString(1, tourType);
             pstmnt.setString(2, price);
             pstmnt.setString(3, personNumber);
             pstmnt.setString(4, hotelType);
-            System.out.println(pstmnt);
             ResultSet rs = pstmnt.executeQuery();
             while (rs.next()) {
                 Tour tour = new Tour(rs.getInt("id"),
@@ -78,12 +79,10 @@ public class TourDAOImpl implements TourDAO {
                         rs.getInt("price"),
                         rs.getInt("hot"),
                         rs.getString("tour_type"),
-                        rs.getInt("hotel_type_id")
+                        rs.getInt("star_rate")
                 );
                 tourList.add(tour);
             }
-//            tourList.forEach(System.out::println);
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
