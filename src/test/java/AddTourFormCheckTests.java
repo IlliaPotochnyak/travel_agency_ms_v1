0ac59fbad59b84bb10ef63c662d34f5443409bb4
IlyaPotochnyak <iluha.diego@gmail.com>
2023-01-24 00:29:31 +0200

    add AddTourFormCheckTests description tests

--- src/test/java/AddTourFormCheckTests.java
+++ src/test/java/AddTourFormCheckTests.java
@@ -108,4 +108,35 @@ public class AddTourFormCheckTests {
         }
     }
 
+    @Test
+    public void AddTourFormTourDescriptionTest () {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+
+        when(request.getParameter("tourName")).thenReturn("Name");
+        when(request.getParameter("tourDescription")).thenReturn("Desc");
+        when(request.getParameter("PersonNumber")).thenReturn("1");
+        when(request.getParameter("tourPrice")).thenReturn("1000");
+        when(request.getParameter("maxDiscount")).thenReturn("10");
+        when(request.getParameter("tourType")).thenReturn("rest");
+        when(request.getParameter("hotelType")).thenReturn("3");
+
+        assertTrue(FormCheckUtils.addTourFormCheck(request));
+
+        String[] descTrue = {"aa", "Aa", "AA", "12", "Abcd", "Abcd abcd.", "!aBcd %_/.12345 ",
+                Stream.generate(() -> "a").limit(65500).collect(Collectors.joining())};
+
+        for (String name: descTrue) {
+            when(request.getParameter("tourDescription")).thenReturn(name);
+            assertTrue(FormCheckUtils.addTourFormCheck(request));
+        }
+
+        String[] descFalse = {"", "a", "A", "1", "!", ".",
+                Stream.generate(() -> "a").limit(65501).collect(Collectors.joining())};
+
+        for (String name: descFalse) {
+            when(request.getParameter("tourDescription")).thenReturn(name);
+            assertFalse(FormCheckUtils.addTourFormCheck(request));
+        }
+    }
+
 }
