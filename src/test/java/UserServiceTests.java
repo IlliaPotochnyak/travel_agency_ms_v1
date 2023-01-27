a97d96d23ac5dbb5c37d5abf0dc789fd9fc91a29
IlyaPotochnyak <iluha.diego@gmail.com>
2023-01-27 13:24:42 +0200

    add loginUserTestPositive tests to UserServiceTests

--- src/test/java/UserServiceTests.java
+++ src/test/java/UserServiceTests.java
@@ -18,14 +18,14 @@ public class UserServiceTests {
 
     User mockedUser = mock(User.class);
 
-    List<User> userList = Arrays.asList(
-            new User(1, "User1", "User1LastName", "user1@mail.com", "+11-111-111-1111", 1, "client"),
-            new User(2, "User2", "User2LastName", "user2@mail.com", "+11-111-111-1111", 1, "client"),
-            new User(3, "User3", "User3LastName", "user3@mail.com", "+11-111-111-1111", 1, "client"),
-            new User(4, "User4", "User4LastName", "user4@mail.com", "+11-111-111-1111", 1, "client"),
-            new User(5, "User5", "User5LastName", "user5@mail.com", "+11-111-111-1111", 1, "client"),
-            new User(6, "User6", "User6LastName", "user6@mail.com", "+11-111-111-1111", 1, "client"),
-            new User(7, "User7", "User7LastName", "user6@mail.com", "+11-111-111-1111", 1, "client")
+    private static final List<User> userList = Arrays.asList(
+            new User(1, "User1", "User1LastName", "user1@mail.com", "password1", "+11-111-111-1111",  "client"),
+            new User(2, "User2", "User2LastName", "user2@mail.com", "password2", "+11-111-111-1111",  "client"),
+            new User(3, "User3", "User3LastName", "user3@mail.com", "password3", "+11-111-111-1111",  "client"),
+            new User(4, "User4", "User4LastName", "user4@mail.com", "password4", "+11-111-111-1111",  "client"),
+            new User(5, "User5", "User5LastName", "user5@mail.com", "password5", "+11-111-111-1111",  "client"),
+            new User(6, "User6", "User6LastName", "user6@mail.com", "password6", "+11-111-111-1111",  "client"),
+            new User(7, "User7", "User7LastName", "user7@mail.com", "password7", "+11-111-111-1111", "client")
             );
 
 
@@ -83,5 +83,28 @@ public class UserServiceTests {
             assertEquals(userDTOList.get(i).getId(), userList.get(i + offset).getId());
             assertEquals(userDTOList.get(i).getFirstName(), userList.get(i + offset).getFirstName());
         }
+    }
+
+    @Test
+    public void loginUserTestPositive() throws DatabaseException {
+        String[] emailArr = {"user1@mail.com", "user3@mail.com", "user7@mail.com"};
+        String[] passwordArr = {"password1", "password3", "password7"};
+//        String email;
+
+        for (int i = 0; i < emailArr.length; i++) {
+            String email = emailArr[i];
+            String password = passwordArr[i];
+            User user = userList.stream().filter(u -> u.getEmail().equals(email))
+                    .findFirst().orElse(null);
+            assertNotNull(user);
+            UserDAOImpl userDAO = mock(UserDAOImpl.class);
+            when(userDAO.getUserByEmail(email)).thenReturn(user);
+
+            UserService userService = new UserService(userDAO);
+            UserDTO userDTO = userService.loginUser(email, password);
+
+            assertNotNull(userDTO);
+        }
+
     }
 }
